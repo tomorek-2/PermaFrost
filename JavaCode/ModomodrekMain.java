07c90c3b96863c4150d52fa8feeaaca4f86eb587
tomorek-2 <omavrina89@gmail.com>
2026-07-12 08:56:02 +0300

    Иницилизация кода, добавление кода

---
+++ JavaCode/ModomodrekMain.java
@@ -0,0 +1,15 @@
+package JavaCode;
+
+import arc.math.geom.Point2;
+import arc.struct.IntSeq;
+import arc.struct.ObjectIntMap;
+import arc.struct.Seq;
+import mindustry.mod.Mod;
+
+public class ModomodrekMain extends Mod {
+    public static ObjectIntMap<Point2> HeatMap = new ObjectIntMap<>();
+    @Override
+    public void init() {
+
+    }
+}
--- JavaCode/world/block/realHeat/BlockHeat.java
+++ JavaCode/world/block/realHeat/BlockHeat.java
@@ -1,4 +1,60 @@
 package world.block.realHeat;
 
-public class BlockHeat {
+import JavaCode.ModomodrekMain;
+import arc.math.Mathf;
+import arc.math.geom.Point2;
+import arc.struct.EnumSet;
+import mindustry.gen.Building;
+import mindustry.type.Item;
+import mindustry.world.Block;
+import mindustry.world.Build;
+import mindustry.world.blocks.storage.StorageBlock;
+import mindustry.world.meta.BlockFlag;
+import mindustry.world.meta.BlockGroup;
+import mindustry.world.meta.Env;
+
+public class BlockHeat extends Block {
+    public BlockHeat(String name){
+        super(name);
+        hasItems = true;
+        solid = true;
+        update = true;
+        sync = true;
+        destructible = true;
+        separateItemCapacity = true;
+        group = BlockGroup.transportation;
+        flags = EnumSet.of(BlockFlag.storage);
+        allowResupply = true;
+        envEnabled = Env.any;
+        drawCached = true;
+        drawDynamic = false;
+    }
+    public class BlockHeatBuildIng extends Building {
+
+        @Override
+        public void updateTile(){
+            int xblock = tile.x;
+            int yblock = tile.y;
+
+            for(int i = 0; i < 700; i += 3) {
+                for(int x = 0; x < 10; x++) {
+                    float xd = Mathf.cosDeg(i) * x + xblock;
+                    float yd = Mathf.sinDeg(i) * x + yblock;
+
+                    int xdint = (int) xd;
+                    int ydint = (int) yd;
+
+
+
+
+
+                    int power = 10 - x;
+                    ModomodrekMain.HeatMap.put(new Point2(xdint, ydint), power);
+
+                }
+            }
+        }
+
+
+    }
 }
